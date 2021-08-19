043181fce122bbc4d6a2c1c89ccff25e94efe978
venzard <vasilij-ch@ya.ru>
2021-08-19 17:54:56 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function reverse (n) {
-  
+    let i=0;
+    let result='';
+    if (n<0) {
+        let str = String(n)
+        for (i = 0; i < str.length-1; i++){
+            result+=str[str.length-i-1];
+            console.log('res',i,'= ',result);
+        }
+        console.log('str= ',str.length);
+        console.log('i=', str[i-1]);
+        return result;
+    }else {
+        let str = String(n)
+        for (i = 0; i < str.length; i++){
+            result+=str[str.length-i-1];
+            console.log('res',i,'= ',result);
+        }
+        console.log('str= ',str.length);
+        console.log('i=', str[i-1]);
+        return result;
+    }
 }
